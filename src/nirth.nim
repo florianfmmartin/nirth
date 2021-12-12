e0fb9ee4222fcec121a4164b98dccbeb680ce3e2
florianfmmartin <florian.fm.martin@gmail.com>
2021-12-12 13:51:27 -0500

    feat: part 1 and part 2

--- src/nirth.nim
+++ src/nirth.nim
@@ -1,5 +1,62 @@
-# This is just an example to get you started. A typical binary package
-# uses this file as the main entry point of the application.
+from std/strutils import parseInt, splitWhitespace
+
+type
+  VM = object
+    stack: seq[int]
+
+proc ctrlc() {.noconv.} =
+  echo("\nGood bye!")
+  quit()
+
+setControlCHook(ctrlc)
+
+proc processInput(vm: var VM, input: string): bool =
+  try:
+    let integer = parseInt(input)
+    vm.stack.add(integer)
+  except ValueError:
+    try:
+      case input:
+        of "show":
+          echo(vm.stack)
+        of "bye":
+          ctrlc()
+        of "+": # (b a -- [a+b])
+          let
+            a = vm.stack.pop()
+            b = vm.stack.pop()
+          vm.stack.add(a + b)
+        else:
+          echo("I don't know what `" & input & "` means...")
+          return false
+    except IndexDefect:
+      echo("Stack underflow by `" & input & "` ...")
+      return false
+  return true
+
+proc main() {.raises: [].} =
+  var
+    vm: VM = VM(stack: @[])
+    input: string
+    input_array: seq[string]
+    ok: bool = true
+
+  echo("Welcome to Nirth!")
+
+  while ok:
+    try:
+      stdout.write(">>>> ")
+      input = readLine(stdin)
+    except IOError:
+      echo("Good bye!")
+
+    input_array = input.splitWhitespace()
+
+    for i in input_array:
+      ok = ok and vm.processInput(i)
+
+    if ok:
+      echo("ok.")
 
 when isMainModule:
-  echo("Hello, World!")
+  main()
