321d92bb5e88e27bb573ecc2f7d3a8c0cabd9b72
florianfmmartin <florian.fm.martin@gmail.com>
2021-12-14 14:48:20 -0500

    feat: part 6

--- src/nirth.nim
+++ src/nirth.nim
@@ -23,88 +23,107 @@ proc endCompile(vm: var VM) =
   vm.compiledWords &= " ;;;; "
   vm.compileFlag = false
 
-proc runWord(vm: VM, word: string) =
-  let
-    formattedWord = ":::: " & word & " "
-    startIndex = vm.compiledWords.find(formattedWord)
-    endIndex = vm.compiledWords.find(";;;;", start=startIndex)
-    wordDefinition = vm.compiledWords[startIndex..<endIndex]
-    wordDefinitionList = wordDefinition.splitWhitespace()
-    definitionList = wordDefinitionList[2..^1]
-  echo(definitionList)
+proc processArrayOfWords(vm: var VM, input_array: seq[string], ok: var bool, silent: bool = false) =
 
-proc processNonCompilingInput(vm: var VM, input: string): bool =
-  case input:
-    of "show":
-      echo(vm.stack)
-    of "bye":
-      ctrlc()
-    of "+": # (b a -- [b+a])
-      let
-        a = vm.stack.pop()
-        b = vm.stack.pop()
-      vm.stack.add(b + a)
-    of "-": # (b a -- [b-a])
-      let
-        a = vm.stack.pop()
-        b = vm.stack.pop()
-      vm.stack.add(b - a)
-    of "*": # (b a -- [b*a])
-      let
-        a = vm.stack.pop()
-        b = vm.stack.pop()
-      vm.stack.add(b * a)
-    of "/": # (b a -- [b/a])
-      let
-        a = vm.stack.pop()
-        b = vm.stack.pop()
-      vm.stack.add(b div a)
-    of "%": # (b a -- [b%a])
-      let
-        a = vm.stack.pop()
-        b = vm.stack.pop()
-      vm.stack.add(b mod a)
-    of "pop": # (b a -- b)
-      let a = vm.stack.pop()
-      echo(a)
-    of ":":
-      let _ = 0
-      vm.startCompile()
-    of "compiler":
-      echo(vm.compiledWords)
-    of ";":
-      echo("Not in compile mode...")
+  #### processInput
+  proc processInput(vm: var VM, input: string): bool =
+
+    #### processNonCompilingInput
+    proc processNonCompilingInput(vm: var VM, input: string): bool =
+      
+      #### runWord
+      proc runWord(vm: var VM, word: string): bool =
+        var ok = true
+        let
+          formattedWord = ":::: " & word & " "
+          startIndex = vm.compiledWords.find(formattedWord)
+          endIndex = vm.compiledWords.find(";;;;", start=startIndex)
+          wordDefinition = vm.compiledWords[startIndex..<endIndex]
+          wordDefinitionList = wordDefinition.splitWhitespace()
+          definitionList = wordDefinitionList[2..^1]
+        vm.processArrayOfWords(definitionList, ok, silent=true)
+        if (not ok):
+          echo("Error caused by `" & word & "`")
+        return ok
+      #### runWord
+
+      case input:
+        of "show":
+          echo(vm.stack)
+        of "bye":
+          ctrlc()
+        of "+": # (b a -- [b+a])
+          let
+            a = vm.stack.pop()
+            b = vm.stack.pop()
+          vm.stack.add(b + a)
+        of "-": # (b a -- [b-a])
+          let
+            a = vm.stack.pop()
+            b = vm.stack.pop()
+          vm.stack.add(b - a)
+        of "*": # (b a -- [b*a])
+          let
+            a = vm.stack.pop()
+            b = vm.stack.pop()
+          vm.stack.add(b * a)
+        of "/": # (b a -- [b/a])
+          let
+            a = vm.stack.pop()
+            b = vm.stack.pop()
+          vm.stack.add(b div a)
+        of "%": # (b a -- [b%a])
+          let
+            a = vm.stack.pop()
+            b = vm.stack.pop()
+          vm.stack.add(b mod a)
+        of "pop": # (b a -- b)
+          let a = vm.stack.pop()
+          echo(a)
+        of ":":
+          let _ = 0
+          vm.startCompile()
+        of "compiler":
+          echo(vm.compiledWords)
+        of ";":
+          echo("Not in compile mode...")
+        else:
+          if (vm.compiledWords.contains((":::: " & input & " "))):
+            # echo("`" & input & "` exists in dictionary...")
+            return vm.runWord(input)
+          else:
+            echo("I don't know what `" & input & "` means...")
+            return false
+      return true
+    #### processNonCompilingInput
+
+    if vm.compileFlag:
+      case input:
+        of "compiler":
+          echo(vm.compiledWords)
+        of ";":
+          let _ = 0
+          vm.endCompile()
+        else:
+          let _ = 0
+          vm.compile(input)
     else:
-      if (vm.compiledWords.contains((":::: " & input & " "))):
-        # echo("`" & input & "` exists in dictionary...")
-        vm.runWord(input)
-      else:
-        echo("I don't know what `" & input & "` means...")
-        return false
-  return true
-
-proc processInput(vm: var VM, input: string): bool =
-  if vm.compileFlag:
-    case input:
-      of "compiler":
-        echo(vm.compiledWords)
-      of ";":
-        let _ = 0
-        vm.endCompile()
-      else:
-        let _ = 0
-        vm.compile(input)
-  else:
-    try:
-      let integer = parseInt(input)
-      vm.stack.add(integer)
-    except ValueError:
       try:
-          return vm.processNonCompilingInput(input)
-      except IndexDefect:
-        echo("Stack underflow by `" & input & "` ...")
-        return false
-  return true
+        let integer = parseInt(input)
+        vm.stack.add(integer)
+      except ValueError:
+        try:
+            return vm.processNonCompilingInput(input)
+        except IndexDefect:
+          echo("Stack underflow by `" & input & "` ...")
+          return false
+    return true
+  #### processInput
+
+  for i in input_array:
+    ok = ok and vm.processInput(i)
+  if ok and (not silent):
+    echo("ok.")
 
 proc main() {.raises: [].} =
   var
@@ -125,11 +144,7 @@ proc main() {.raises: [].} =
 
     input_array = input.splitWhitespace()
 
-    for i in input_array:
-      ok = ok and vm.processInput(i)
-
-    if ok:
-      echo("ok.")
+    vm.processArrayOfWords(input_array, ok)
 
 when isMainModule:
   main()
